c27ccc6e126e1290f32255626a8616f4c73af9ba
Ethan Henson <147562282+EEEHenson@users.noreply.github.com>
2024-02-10 14:57:07 -0600

    Update Moves.java

--- Moves.java
+++ Moves.java
@@ -1,10 +1,12 @@
 public class Moves {
+  static long FILE_A=72340172838076673L;
+  static long FILE_H=-9187201950435737472L;
 
   public static long possibleMovesWP(long WP) {
     return WP >>> 8;
   }
 
   public static long possibleMovesBP(long BP) {
-    reurn BP << 8;
+    return BP << 8;
   }
 }
