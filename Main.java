efc233c8dbf0e2611dde5e2f774737e1eef849a5
Ethan Henson <147562282+EEEHenson@users.noreply.github.com>
2024-02-18 11:48:31 -0600

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,5 @@
+public class Main {
+  public static void main(String[] args){
+    BitBoards.initGame(); // calls initGame to start the game right now it is used for debugging move calculation
+  }
+}
