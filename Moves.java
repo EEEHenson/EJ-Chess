856ac5722e72cace3f07327768747f280850bb56
Ethan Henson <147562282+EEEHenson@users.noreply.github.com>
2024-02-25 13:30:51 -0600

    Update Moves.java

--- Moves.java
+++ Moves.java
@@ -14,35 +14,35 @@ public class Moves {
 	static long NOT_MY_PIECES;
 	static long MY_PIECES;
 	
-	static long RankMasks8[] =/*from rank1 to rank8*/
+	static long RankMasks[] =//from rank1 to rank8
 	    {
 	        0xFFL, 0xFF00L, 0xFF0000L, 0xFF000000L, 0xFF00000000L, 0xFF0000000000L, 0xFF000000000000L, 0xFF00000000000000L
 	    };
-	    static long FileMasks8[] =/*from fileA to FileH*/
+	    static long FileMasks[] =//from fileA to FileH
 	    {
 	        0x101010101010101L, 0x202020202020202L, 0x404040404040404L, 0x808080808080808L,
 	        0x1010101010101010L, 0x2020202020202020L, 0x4040404040404040L, 0x8080808080808080L
 	    };
-	    static long DiagonalMasks8[] =/*from top left to bottom right*/
+	    static long DiagonalMasks[] =//from top left to bottom right
 	    {
 		0x1L, 0x102L, 0x10204L, 0x1020408L, 0x102040810L, 0x10204081020L, 0x1020408102040L,
 		0x102040810204080L, 0x204081020408000L, 0x408102040800000L, 0x810204080000000L,
 		0x1020408000000000L, 0x2040800000000000L, 0x4080000000000000L, 0x8000000000000000L
 	    };
-	    static long AntiDiagonalMasks8[] =/*from top right to bottom left*/
+	    static long AntiDiagonalMasks[] =//from top right to bottom left
 	    {
 		0x80L, 0x8040L, 0x804020L, 0x80402010L, 0x8040201008L, 0x804020100804L, 0x80402010080402L,
 		0x8040201008040201L, 0x4020100804020100L, 0x2010080402010000L, 0x1008040201000000L,
 		0x804020100000000L, 0x402010000000000L, 0x201000000000000L, 0x100000000000000L
 	    };
 	
-	static long HAndVMoves(int s) {
+	static long HorizontalAndVertical(int s) {
         long binaryS=1L<<s;
         long possibilitiesHorizontal = (OCCUPIED - 2 * binaryS) ^ Long.reverse(Long.reverse(OCCUPIED) - 2 * Long.reverse(binaryS));
         long possibilitiesVertical = ((OCCUPIED&FileMasks8[s % 8]) - (2 * binaryS)) ^ Long.reverse(Long.reverse(OCCUPIED&FileMasks8[s % 8]) - (2 * Long.reverse(binaryS)));
         return (possibilitiesHorizontal&RankMasks8[s / 8]) | (possibilitiesVertical&FileMasks8[s % 8]);
     }
-    static long DAndAntiDMoves(int s) {
+    static long DiagonalAndADiagonal(int s) {
         long binaryS=1L<<s;
         long possibilitiesDiagonal = ((OCCUPIED&DiagonalMasks8[(s / 8) + (s % 8)]) - (2 * binaryS)) ^ Long.reverse(Long.reverse(OCCUPIED&DiagonalMasks8[(s / 8) + (s % 8)]) - (2 * Long.reverse(binaryS)));
         long possibilitiesAntiDiagonal = ((OCCUPIED&AntiDiagonalMasks8[(s / 8) + 7 - (s % 8)]) - (2 * binaryS)) ^ Long.reverse(Long.reverse(OCCUPIED&AntiDiagonalMasks8[(s / 8) + 7 - (s % 8)]) - (2 * Long.reverse(binaryS)));
