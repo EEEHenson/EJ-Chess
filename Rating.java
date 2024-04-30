5f5f2a22b821883414589f811acd60695f3f3fb5
Ethan Henson <147562282+EEEHenson@users.noreply.github.com>
2024-04-30 17:37:42 -0500

    Update Rating.java

--- Rating.java
+++ Rating.java
@@ -1,5 +1,7 @@
+package elor;
+
 public class Rating {
-	public static int evaluate() {
-		return 0;
+	public static int evaluate(String moves,long WP,long WN,long WB,long WR,long WQ,long WK,long BP,long BN,long BB,long BR,long BQ,long BK,long EP,boolean CWK,boolean CWQ,boolean CBK,boolean CBQ,boolean WhiteToMove) {
+		return Search.getFirstLegalMove(moves,WP,WN,WB,WR,WQ,WK,BP,BN,BB,BR,BQ,BK,EP,CWK,CWQ,CBK,CBQ,WhiteToMove);
 	}
 }
